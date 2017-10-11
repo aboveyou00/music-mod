4b689b503b9cb4d3404ba48a062a8881e5b78145
aboveyou00 <brandonyoyoslade@gmail.com>
2017-10-11 09:17:04 -0600

    feat(music-mod): rename example mod to 'Music Mod'

--- README.md
+++ README.md
@@ -4,10 +4,30 @@ A Minecraft mod created for a school project at Neumont College of Computer Scie
 
 ## Development
 
+### Installation
+
 Install the JDK for java 1.8. Install Eclipse. Clone this repository.
 Run these commands:
 
 ```bash
 ./gradlew setupDecompWorkspace
 ./gradlew eclipse
+```
+
+### Build
+
+```bash
+./gradlew build
+```
+
+### Run client
+
+```bash
+./gradlew runClient
+```
+
+### Run server
+
+```bash
+./gradlew runServer
 ```
--- src/main/java/com/blslade/music_mod/MusicMod.java
+++ src/main/java/com/blslade/music_mod/MusicMod.java
@@ -1,20 +1,20 @@
-package com.example.examplemod;
+package com.blslade.music_mod;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = MusicMod.MODID, version = MusicMod.VERSION)
+public class MusicMod
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "musicmod";
     public static final String VERSION = "1.0";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
         // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+        System.out.println("DIRT BLOCK >> " + Blocks.DIRT.getUnlocalizedName());
     }
 }
